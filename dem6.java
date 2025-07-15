a9718aae1d8b42a2557e1c3f3546d528668c3909
sheshu-kvs <kvs24161@gmail.com>
2025-07-15 11:18:08 +0530

    Hello

--- dem6.java
+++ dem6.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class dem6 {
     public static void main(String[] args){
-        System.out.println("Hello");
+        Scanner scan=new Scanner(System.in);
+        int num=scan.nextInt();
+        for(int i=0;i<=num;i++){
+            if((i==6) && (i==7) && (i==8)){
+            break;
+           }
+            else{
+            System.out.print(i+" ");
+            }
+        }
     }
     
 }
